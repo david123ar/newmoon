f58cc74ba8b0ccc7a8c446b70d0cf2bc2f01c1c1
rishabh-star <risingmerta@gmail.com>
2025-01-27 10:41:47 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  export default nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true,
+    disable: true,  // Disable linting entirely for builds
+  },
+};
+
+export default nextConfig;
+
   
