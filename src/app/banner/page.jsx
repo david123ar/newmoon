4c76449635c4c6fe8998c9b938ffed99345cca89
rishabh-star <risingmerta@gmail.com>
2025-02-23 21:29:40 +0530

    first commit

---
+++ src/app/banner/page.jsx
@@ -0,0 +1,40 @@
+import Script from "next/script";
+
+const BannerAd = () => {
+  return (
+    <div
+      style={{
+        width: "468px",
+        height: "60px",
+        margin: "20px auto",
+        textAlign: "center",
+        backgroundColor: "#f0f0f0", // Temporary background to check div visibility
+      }}
+    >
+      {/* Adsterra configuration */}
+      <Script
+        id="adsterra-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : '3e5c0db0e54f3f6872ff8546641e31c0',
+              'format' : 'iframe',
+              'height' : 60,
+              'width' : 468,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+
+      {/* Adsterra script */}
+      <Script
+        src="//disgustingmad.com/3e5c0db0e54f3f6872ff8546641e31c0/invoke.js"
+        strategy="afterInteractive"
+      />
+    </div>
+  );
+};
+
+export default BannerAd;
