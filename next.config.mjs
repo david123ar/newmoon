500ece85647e0cea77716d14001176b209e23dd0
rishabh-star <risingmerta@gmail.com>
2025-02-23 22:52:07 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,42 +4,44 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
-  // Increase static page generation timeout to 300 seconds
   staticPageGenerationTimeout: 3000,
 
-  // Add image domain configuration for external image loading
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.noitatnemucod.net',
-        pathname: '/**',  // Allow all paths under this hostname
+        protocol: "https",
+        hostname: "cdn.noitatnemucod.net",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'img.zorores.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "img.zorores.com",
+        pathname: "/**",
       },
     ],
   },
 
-  // Add HTTP headers configuration
   async headers() {
     return [
       {
-        source: '/:path*', // Apply to all paths
+        source: "/:path*", // Apply to all paths
         headers: [
           {
-            key: 'X-Robots-Tag',
-            value: 'index, follow', // Allow indexing for all pages
+            key: "X-Robots-Tag",
+            value: "index, follow", // Allow indexing for all pages
           },
         ],
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots",
       },
     ];
   },
