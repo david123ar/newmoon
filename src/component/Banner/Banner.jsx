e7983ef0246e67472efb408a91e6b19b90f21e62
rishabh-star <risingmerta@gmail.com>
2025-02-23 19:35:27 +0530

    first commit

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,30 +1,38 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import Script from "next/script";
+import React from "react";
 
 const BannerAd = () => {
-  const adRef = useRef(null);
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && adRef.current && !adRef.current.hasChildNodes()) {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = "//disgustingmad.com/3e5c0db0e54f3f6872ff8546641e31c0/invoke.js";
-      script.async = true;
-      adRef.current.appendChild(script);
-    }
-  }, []);
-
   return (
     <div
-      ref={adRef}
       style={{
         width: "468px",
         height: "60px",
         margin: "20px auto",
         textAlign: "center",
+        backgroundColor: "#f0f0f0", // Temporary background to check visibility
       }}
     >
-      {/* Adsterra Banner will load here */}
+      <Script
+        id="adsterra-banner"
+        strategy="afterInteractive" // Ensures it loads after page hydration
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : '3e5c0db0e54f3f6872ff8546641e31c0',
+              'format' : 'iframe',
+              'height' : 60,
+              'width' : 468,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+      <Script
+        id="adsterra-script"
+        strategy="afterInteractive"
+        src="//disgustingmad.com/3e5c0db0e54f3f6872ff8546641e31c0/invoke.js"
+      />
     </div>
   );
 };
